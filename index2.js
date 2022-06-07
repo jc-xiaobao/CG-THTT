5615bba9e5e72bd5f6b35eadf9390c3b4e6929c7
jc-xiaobao <105725856+jc-xiaobao@users.noreply.github.com>
2022-06-08 01:40:15 +0700

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,9 @@
+alert('Đây là ứng dụng tính diện tích hình chữ nhật');
+
+let lenght = parseInt(prompt('Vui lòng nhập chiều dài'));
+let width = parseInt(prompt('Vui lòng nhập chiều rộng'));
+
+let area = lenght * width;
+
+alert('dien tich hinh chu nhat la: ' + area);
+
